98baabc43814d0e16b2098080465941f936224f5
kolluru sridhar <87171968+sridharshree303@users.noreply.github.com>
2022-07-15 17:33:39 +0530

    c

--- src/main/java/com/xender/practical/AuthApplication.java
+++ src/main/java/com/xender/practical/AuthApplication.java
@@ -7,11 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AuthApplication {
-
 	public static void main(String[] args) {
 		System.out.println("Start");
 		SpringApplication.run(AuthApplication.class, args);
 		System.out.println("End");
 	}
-	
 }
